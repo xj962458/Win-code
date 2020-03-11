e32ceb5a049401658b8ec9824f17eb3576414100
xj962458 <2291921855@qq.com>
2020-03-11 08:34:16 +0800

    xj

---
+++ xj/xj.h
@@ -0,0 +1,17 @@
+CComboBox cbx_major;//专业关联的控件变量
+int int_sex;        //性别关联的变量
+CString name;       //姓名关联的变量
+
+CComboBox cbx_major;//专业关联的控件变量
+int int_sex;        //性别关联的变量
+CString name;       //姓名关联的变量
+
+BOOL CdialogDlg::OnInitDialog()
+{
+		CDialog::OnInitDialog();
+	
+		this->cbx_major.SetCurSel(2);//默认选择第项，即计算机
+
+		// 将“关于...”菜单项添加到系统菜单中。
+…
+}
